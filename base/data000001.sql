8aec1a247ce8d86442832d90919c00ba08832ed0
Jaime Rivera <jriverarojas@gmail.com>
2015-06-20 13:56:55 -0400

    aplicando integracion con almacenes

--- base/data000001.sql
+++ base/data000001.sql
@@ -9,7 +9,11 @@ select pxp.f_insert_tgui ('SISTEMA DE VENTAS', '', 'VEF', 'si', 1, '', 1, '', ''
 
 /***********************************I-DAT-JRR-VEF-0-05/07/2015*****************************************/
 
+select pxp.f_insert_tgui ('Ventas', 'Ventas', 'VFVENTA', 'si', 1, 'sis_ventas_farmacia/vista/venta/VentaVendedor.php', 2, '', 'VentaVendedor', 'VEF');
 select pxp.f_insert_tgui ('Revisión de Venta', 'Revisión de Venta', 'REVVEN', 'si', 1, 'sis_ventas_farmacia/vista/venta/VentaRevision.php', 2, '', 'VentaRevision', 'VEF');
 select pxp.f_insert_tgui ('Elaboración de Formulas', 'Elaboración de Formulas', 'VENELABO', 'si', 1, 'sis_ventas_farmacia/vista/venta/VentaRevision.php', 2, '', 'VentaRevision', 'VEF');
 
+select pxp.f_insert_testructura_gui ('VFVENTA', 'VEF');
+select pxp.f_insert_testructura_gui ('REVVEN', 'VEF');
+select pxp.f_insert_testructura_gui ('VENELABO', 'VEF');
 /***********************************F-DAT-JRR-VEF-0-05/07/2015*****************************************/
