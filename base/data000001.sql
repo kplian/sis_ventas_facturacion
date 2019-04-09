5333cd3721105dd0ba76ffb9af45ea1c2a52f8f1
JOSE ABRAHAM MITA HUANCA <jose.mita@gmail.com>
2019-04-09 15:42:04 -0400

    change data file

--- base/data000001.sql
+++ base/data000001.sql
@@ -1,7 +1,7 @@
 /***********************************I-DAT-JRR-VEF-0-02/05/2015*****************************************/
 
 INSERT INTO segu.tsubsistema ("codigo", "nombre", "fecha_reg", "prefijo", "estado_reg", "nombre_carpeta", "id_subsis_orig")
-VALUES (E'VEF', E'Sistema de Ventas', E'2015-04-20', E'VF', E'activo', E'ventas_facturacion', NULL);
+VALUES (E'VEF', E'Sistema de Ventas', E'2015-04-20', E'VF', E'activo', E'ventas_facturacion', NULL) ON CONFLICT  (codigo) WHERE estado_reg = 'activo' DO NOTHING;
 
 select pxp.f_insert_tgui ('SISTEMA DE VENTAS', '', 'VEF', 'si', 1, '', 1, '', '', 'VEF');
 
