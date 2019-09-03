7a53d2b5a8ffc055b520cd377d1fb2cb68657b83
Rodrigo Chumacero <rodrychm@gmail.com>
2019-09-03 15:14:43 -0400

    #5 actualizacion

--- reportes/RFacturaReciboPdf.php
+++ reportes/RFacturaReciboPdf.php
@@ -93,7 +93,7 @@ class RFacturaReciboPdf extends  ReportePDF {
 			$this->SetAutoPageBreak(true, 60);
 			}
 			elseif($this->codigo_reporte =='RECIBOETR') {//#5
-			$this->SetHeaderMargin(7); //margin de header de top
+			$this->SetHeaderMargin(3); //margin de header de top
 			$this->SetFooterMargin(60); //margin de Footer de botton
 			$this->SetAutoPageBreak(true, 60);	
 			}
