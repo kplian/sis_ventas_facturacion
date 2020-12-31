72865970dec47aef8f5dd432fe9d5c9016efeddb
mguerra <manuel.guerra@endetransmision.bo>
2020-12-31 11:24:10 -0400

    #ETR-2389 cambio de glosa

--- base/dataupdate000001.sql
+++ base/dataupdate000001.sql
@@ -1,3 +1,12 @@
 /********************************************I-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
 --SHEMA : Esquema (CONTA) contabilidad         AUTHOR:Siglas del autor de los scripts' dataupdate000001.txt
 /********************************************F-DAUP-AUTOR-SCHEMA-0-31/02/2019********************************************/
+
+
+/********************************************I-DAUP-MGM-VEF-0-31/12/2020********************************************/
+
+--rollback
+---UPDATE vef.tventa_detalle SET descripcion='ARRENDAMIENTO OFICINAS UBICADAS EN LA CALLE COLOMBIA Nº O-0655 ENTRE FALSURI Y SUIPACHA, ZONA NOROESTE, COCHABAMBA. SEGÚN CONTRATO Nº 16/2020, POR EL MES DE DICIEMBRE/202020' WHERE id_venta_detalle =2105;
+--commit
+UPDATE vef.tventa_detalle SET descripcion='ARRENDAMIENTO OFICINAS UBICADAS EN LA CALLE COLOMBIA Nº O-0655 ENTRE FALSURI Y SUIPACHA, ZONA NOROESTE, COCHABAMBA. SEGÚN CONTRATO Nº 16/2020, POR EL MES DE DICIEMBRE/2020' WHERE id_venta_detalle =2105;
+/********************************************F-DAUP-MGM-VEF-0-31/12/2020********************************************/
